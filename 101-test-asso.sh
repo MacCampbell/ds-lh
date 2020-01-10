aaa4b606d7d71b7e5fe4d42de0103263c136b975
MacCampbell <campbellmatthewa@gmail.com>
2020-01-10 10:10:07 -0900

    Created basic association test and updated README

---
+++ 101-test-asso.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+# NOTE the -l flag!
+
+# This script is run some test associations on ds lh data.
+
+# Name of the job 
+#SBATCH -J doAsso
+
+#Email myself
+#SBATCH --mail-user=maccampbell@ucdavis.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+#Job level
+
+#SBATCH --partition=high
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks=24
+#SBATCH --time=1-01:00:00 #run for a day and an hour
+
+# Standard out and Standard Error output files with the job number in the name.
+#SBATCH -o bench-%j.output
+#SBATCH -e bench-%j.output
+
+# hostname is just for debugging
+hostname
+export OMP_NUM_THREADS=$SLURM_NTASKS
+module load benchmarks
+
+# The main job executable to run: note the use of srun before it
+# Using $HOME for better practice
+srun $HOME/angsd/angsd -P 24 -yBin $HOME/ds-lh/phenos/sub.phenos -r $HOME/ds-lh/genome/smelt-genome.fasta -minMapQ 30 -minQ 20 -minInd 27 -doAsso 1 -GL 1 -out $HOME/ds-lh/outputs/100/sub -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -bam $HOME/ds-lh/bamlist/sub.bamlist 
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ds-lh
 Delta Smelt life history genetic association testing
+
+## Structure of Files
+
+I have used links to point to files that already have been prepared.    
+
+__1__ /genome/ Hypomesus-transpacificus_10X_F_A.pseudohap2.1.fasta -> /group/millermrgrp2/shannon/projects/genome_Hypomesus-transpacificus/data/04-supernova_out/60x/A/Hypomesus-transpacificus_10X_F_A.pseudohap2.1.fasta     
+
+__2__ /bams/ Ht33-95_2017_G12.sort-n.fixmate-m.sort.markdup-r.bam -> /group/millermrgrp2/shannon/projects/DS_history/data/10X_alignments/Ht33-95_2017_G12.sort-n.fixmate-m.sort.markdup-r.bam (and indices)     
+
+__3__ /outputs/ Are meant to have outputs for files in series 1xx, 2xx and so on (outputs/100 and outputs/200 for example)
+
