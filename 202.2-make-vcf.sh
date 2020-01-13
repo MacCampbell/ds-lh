bfe991d81a5d8926362163f1baa4da96a096422d
MacCampbell <campbellmatthewa@gmail.com>
2020-01-13 10:33:33 -0900

    Creating a *.vcf

---
+++ 202.2-make-vcf.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+# NOTE the -l flag!
+
+# This script is run some test associations on ds lh data.
+
+# Name of the job 
+#SBATCH -J doVcf
+
+#Email myself
+#SBATCH --mail-user=maccampbell@ucdavis.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+#Job level
+
+#SBATCH --partition=high
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks=24
+#SBATCH --time=1-01:00:00 #run for a day and an hour
+
+# Standard out and Standard Error output files with the job number in the name.
+#SBATCH -o bench-%j.output
+#SBATCH -e bench-%j.output
+
+# hostname is just for debugging
+hostname
+export OMP_NUM_THREADS=$SLURM_NTASKS
+module load benchmarks
+
+# The main job executable to run: note the use of srun before it
+# Using $HOME for better practice
+
+$HOME/angsd/angsd -P 24 -b $HOME/ds-lh/bamlists/partial.bamlist -minInd 196  -out $HOME/ds-lh/outputs/200/partial-vcf \
+-minMaf 0.05 -minMapQ 30 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 \
+-SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -doVcf 1 
+
