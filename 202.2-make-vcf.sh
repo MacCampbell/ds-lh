461f11f7f6701788212c17c0d84cff9afa02dd9f
MacCampbell <campbellmatthewa@gmail.com>
2020-01-13 10:42:12 -0900

    Used binary flag not tabular flag.

--- 202.2-make-vcf.sh
+++ 202.2-make-vcf.sh
@@ -34,5 +34,5 @@ module load benchmarks
 
 srun $HOME/angsd/angsd -P 24 -b $HOME/ds-lh/bamlists/partial.bamlist -minInd 196 -out $HOME/ds-lh/outputs/200/partial-vcf \
 -minMaf 0.05 -minMapQ 30 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 \
--SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -doPlink 1
+-SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -doPlink 2
 
