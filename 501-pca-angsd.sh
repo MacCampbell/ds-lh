3be2711eda22a91ccd93d6a14ac37680a65c8436
MacCampbell <campbellmatthewa@gmail.com>
2020-01-24 10:20:16 -0900

    Error in pca-angsd

--- 501-pca-angsd.sh
+++ 501-pca-angsd.sh
@@ -16,8 +16,8 @@
 
 #SBATCH --partition=high
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=6
-#SBATCH --ntasks=12
+#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks=10
 #SBATCH --time=1-08:00:00 #run for a day and an eight hours
 
 # Standard out and Standard Error output files with the job number in the name.
@@ -30,10 +30,10 @@ export OMP_NUM_THREADS=$SLURM_NTASKS
 module load benchmarks
 
 #Doing the calculations making sure sites are present in 90% of individuals.
-srun $HOME/angsd/angsd -minInd 194 -GL 1 -out $HOME/ds-lh/outputs/500/216-pca -nThreads 12 -doGlf 2 -doMajorMinor 1 -doMaf 2 -SNP_pval 1e-6 -minMapQ 30 -minQ 20 \
+srun $HOME/angsd/angsd -minInd 194 -GL 1 -out $HOME/ds-lh/outputs/500/216-pca -nThreads 10 -doGlf 2 -doMajorMinor 1 -doMaf 2 -SNP_pval 1e-6 -minMapQ 30 -minQ 20 \
 -bam $HOME/ds-lh/bamlists/216.bamlist
 
 # Generate a covariance matrix
 # For Linux
-srun python $HOME/pcangsd/pcangsd.py -beagle $HOME/outputs/500/216-pca.beagle.gz -admix -o $HOME/ds-lh/outputs/500/216-pca
+srun python $HOME/pcangsd/pcangsd.py -beagle $HOME/ds-lh/outputs/500/216-pca.beagle.gz -admix -o $HOME/ds-lh/outputs/500/216-pca
 
